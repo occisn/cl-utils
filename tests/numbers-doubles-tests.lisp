f0aeea0376a7472d8674926bb79a9055f34cc02c
Nicolas O <noccis@I7413>
2025-12-23 14:24:02 +0100

    Fix detail

--- tests/numbers-doubles-tests.lisp
+++ tests/numbers-doubles-tests.lisp
@@ -9,9 +9,9 @@
   (parachute:is = 42 (floor-of-positive-df 42.999999d0))
 
   (loop for x in '(0.1d0 0.5d0 0.9d0)
-        do (parachute:is = 0 (floor-of-positive-df x)))
+        do (parachute:true (= 0 (floor-of-positive-df x))))
 
   (loop for x in '(10.1d0 10.5d0 10.9d0)
-        do (parachute:is = 10 (floor-of-positive-df x))))
+        do (parachute:true (= 10 (floor-of-positive-df x)))))
 
 ;; end
