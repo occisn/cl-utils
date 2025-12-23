625c1ad7f1061ae0c78ff666a6007785bb021f8a
Nicolas O <noccis@I7413>
2025-12-23 14:32:53 +0100

    Improve 'reverse-number' function

--- src/numbers-integers.lisp
+++ src/numbers-integers.lisp
@@ -6,12 +6,13 @@ For instance: 123 --> 321.
 (v1, available in occisn/cl-utils GitHub repository)"
   (declare (type fixnum n))
   (labels ((sub (num acc)
-             (declare (type fixnum acc))
+             (declare (type fixnum num acc))
              (if (= num 0)
 	         acc
                  (multiple-value-bind (quotient remainder)
                      (floor num 10)
-                   (sub quotient (the fixnum (+ (* 10 acc) remainder)))))))
+                   (declare (type fixnum quotient remainder))
+                   (sub quotient (the fixnum (+ (the fixnum (* 10 acc)) remainder)))))))
     (sub n 0)))
 
 ;; end
