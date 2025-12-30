40ab5012fe9327c60a0189373874a74ae24649db
Nicolas O <noccis@I7413>
2025-12-30 21:44:37 +0100

    Improve explanation around optimization compilation notes

--- src/optimization.lisp
+++ src/optimization.lisp
@@ -11,6 +11,10 @@
     (setq tmp (the fixnum (expt m (the (integer 0) (- n 2)))))
     ;; (integer 0) can be replaced by unsigned-byte
 
+    ;; ceiling
+    ;; (the fixnum (ceiling...
+    ;; reason: The return type of ceiling in CL is always (values integer remainder) where integer is a general integer (not necessarily a fixnum)
+    
     ;; parachute
     ;; (locally
     ;;     (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
