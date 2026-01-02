e3f35e3aa3e794a5564aaf4e44b34e19a6824b61
Nicolas O <noccis@I7413>
2026-01-02 20:06:33 +0100

    Add information about symbols

--- src/symbols.lisp
+++ src/symbols.lisp
@@ -1,5 +1,7 @@
 (in-package :cl-utils)
 
+;; General explanations at the end of the file.
+
 (defun function-to-string (fn)
   "Return a string corresponding to function FN.
 For instance #'SIN --> 'SIN'
@@ -46,4 +48,128 @@ For instance: 'abc::def' #\: --> 'def'
   (format t "~%")
   (format t "#'sin --> ~a~%" (function-to-string #'sin)))
 
+
+;; A symbol is a name that can refer to values, functions, properties,
+;; and more — all at the same time.
+;; It is also a first-class object that we can manipulate.
+
+;; It has 5 components:
+;; Name: A string that identifies the symbol (like FOO or MY-VARIABLE)
+;; Value cell: Can hold a value when the symbol is used as a variable
+;; Function cell: Can hold a function definition when the symbol names a function
+;; Property list: A list of key-value pairs for storing additional attributes
+;; Package: The package to which the symbol belongs
+
+;; Symbols are "interned" in packages, meaning that within a given package,
+;; there's only one symbol with a particular name. When you type FOO twice,
+;; you get the same symbol object both times. 
+
+(locally
+
+    (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
+
+  (defvar *foo8* 5)
+  ;; IMPORTANT: we use a DYNAMIC variable
+  ;; since LEXICAL variables (let bindings) are resolved
+  ;; at compile time and don't use the symbol's value cell
+
+  (defun *foo8* (n) (* 2 n))
+
+  ;; is a symbol?
+  ;; ------------
+  (symbolp '*foo8*)                      ; => T
+
+  ;; name:
+  ;; -----
+  (symbol-name '*foo8*)                  ; ==> "*FOO8*"
+
+  ;; package:
+  ;; --------
+  (symbol-package '*foo8*)          ; ==> #<PACKAGE "COMMON-LISP-USER">
+
+  ;; value:
+  ;; ------
+  *foo8*                                 ; ==> 5
+  (symbol-value '*foo8*)                 ; ==> 5
+  (boundp '*foo8*)                       ; => T (if *foo8* has a value)
+  (set '*foo8* 10)
+  *foo8*                                 ; ==> 10
+  (setf *foo8* 20)
+  *foo8*                                 ; ==> 20
+  (setf (symbol-value '*foo8*) 30)
+  *foo8*                                 ; ==> 30
+
+  ;; function:
+  ;; ---------
+  (fboundp '*foo8*)            ; => T (if *foo8* has a function binding)
+  (symbol-function '*foo8*)    ; ==> #<FUNCTION *FOO8*>
+  (setf (symbol-function '*foo8*) (lambda (n) (* 3 n)))
+  (*foo8* 5)                             ; ==> 15
+
+  ;; mix of value and function:
+  ;; --------------------------
+  (*foo8* *foo8*)                         ; ==> 90 (30 x 3)
+  (funcall #'*foo8* *foo8*)               ; ==> 90
+  (apply #'*foo8* (list *foo8*))          ; ==> 90
+
+  ;; property list:
+  ;; --------------
+  (symbol-plist '*foo8*)                 ; ==> NIL
+  (setf (get '*foo8* 'color) 'red)
+  (get '*foo8* 'color)                   ; ==> RED
+  (symbol-plist '*foo8*)                 ; ==> (COLOR RED)
+
+  ;; full content:
+  ;; -------------
+
+  ;; (1/3)
+  ;; (describe '*foo8*)
+  ;; ==>
+  ;; COMMON-LISP-USER::*FOO8*
+  ;;   [symbol]
+  ;;
+  ;; *FOO8* names a special variable:
+  ;;   Value: 30
+  ;;
+  ;; *FOO8* names a compiled function:
+  ;;   Lambda-list: (N)
+  ;;   Derived type: (FUNCTION (T) (VALUES NUMBER &OPTIONAL))
+  ;;   Source file: c:/Users/noccis/Downloads/tmp.lisp
+  ;;
+  ;; Symbol-plist:
+  ;;   COLOR -> RED
+
+  ;; (2/3)
+  ;; (inspect '*foo8*) ; then q
+  ;; ==>
+  ;; The object is a SYMBOL.
+  ;; 0. Name: "*FOO8*"
+  ;; 1. Package: #<PACKAGE "COMMON-LISP-USER">
+  ;; 2. Value: 30
+  ;; 3. Function: #<FUNCTION (LAMBDA (N) :IN "c:/Users/noccis/Downloads/tmp.lisp") {2264ED3B}>
+  ;; 4. Plist: (COLOR RED)
+
+  ;; (3/3)
+  ;; C-c I (do not forget the quote)
+  ;; ==>
+  ;; #<SYMBOL {20322BAF}>
+  ;; --------------------
+  ;; Its name is: "*FOO8*"
+  ;; It is a global variable bound to: 30 [unbind]
+  ;; It is a function: #<FUNCTION (LAMBDA (N) :IN "c:/Users/noccis/Downloads/tmp.lisp") {2264ED3B}> [unbind]
+  ;; It is internal to the package: COMMON-LISP-USER [export] [unintern]
+  ;; Property list: (COLOR RED)
+
+  
+  ;; interned or uninterned symbols:
+  ;; -------------------------------
+  (make-symbol "TEMP9")      ; => #:TEMP (uninterned)
+  (gensym)                  ; => #:G1234 (unique, uninterned)
+  (gensym "VAR") ; => #:VAR1235
+  (intern "FOO9")           ; => FOO, :INTERNAL
+  (unintern 'my-symbol4)
+
+  )
+
 ;;; end
+
