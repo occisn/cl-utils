a2fb35041cf131f265f63a7d2adc526a6b056fc3
Nicolas O <20554537+occisn@users.noreply.github.com>
2026-03-19 16:38:27 +0100

    Add S-expression utilities and tests

---
+++ src/sexp.lisp
@@ -0,0 +1,38 @@
+(in-package :cl-utils)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun sexp-replace-sexp-beginning-by (lst prefix replace-builder-fn)
+    "Transform sexp LST in the following way: each time a sexp, called sexp1, is found within LST of format sexp1 = (PREFIX...), it is replaced by (REPLACE-BUILDER-FN sexp1).
+
+Used by macros.
+
+Example:
+(sexp-replace-sexp-beginning-by
+   '(a (d (submit g h)) c)
+   'submit
+   (lambda (sexp) (cons 'submit2 (cdr sexp)))))
+;;; --> (A (D (SUBMIT2 G H)) C)"
+    (declare (type (or atom list) lst)
+             (type function replace-builder-fn))
+    (cond ((atom lst) lst)
+          ((list lst)
+           (if (eq prefix (car lst))
+               (funcall replace-builder-fn lst)
+               (loop for elt in lst collect (sexp-replace-sexp-beginning-by elt prefix replace-builder-fn)))))))
+;;; eval-when sinon "It is defined earlier in the file but is not available at compile-time"
+
+(defun SHOW-sexp-replace-sexp-beginning-by ()
+  "Example of usage of sexp-replace-sexp-beginning-by."
+  (sexp-replace-sexp-beginning-by
+   '(a (d (submit g h)) c)
+   'submit
+   (lambda (sexp) (cons 'submit2 (cdr sexp)))))
+;;; --> (A (D (SUBMIT2 G H)) C)
+
+(defun SHOW-all-sexp ()
+  ""
+  (format t "~%~%======~%=== SEXP~%======~%")
+  (format t "~%")
+  (format t "~a~%" (SHOW-sexp-replace-sexp-beginning-by)))
+
+;;; end
---
+++ tests/sexp-tests.lisp
@@ -0,0 +1,55 @@
+(in-package :cl-utils-tests)
+
+;;; === sexp-replace-sexp-beginning-by
+
+(parachute:define-test sexp-replace-sexp-beginning-by-test
+
+  ;; Basic replacement (the docstring example)
+  (parachute:is equal
+                '(a (d (submit2 g h)) c)
+                (sexp-replace-sexp-beginning-by
+                 '(a (d (submit g h)) c)
+                 'submit
+                 (lambda (sexp) (cons 'submit2 (cdr sexp)))))
+
+  ;; No match: nothing changes
+  (parachute:is equal
+                '(a (b c) d)
+                (sexp-replace-sexp-beginning-by
+                 '(a (b c) d)
+                 'z
+                 (lambda (sexp) (cons 'replaced (cdr sexp)))))
+
+  ;; Match at top level
+  (parachute:is equal
+                '(new b c)
+                (sexp-replace-sexp-beginning-by
+                 '(old b c)
+                 'old
+                 (lambda (sexp) (cons 'new (cdr sexp)))))
+
+  ;; Multiple matches: replacement stops recursion into matched sexp
+  (parachute:is equal
+                '((y 1) (y (x 2)))
+                (sexp-replace-sexp-beginning-by
+                 '((x 1) (x (x 2)))
+                 'x
+                 (lambda (sexp) (cons 'y (cdr sexp)))))
+
+  ;; Atom input: returned as-is
+  (parachute:is eq
+                42
+                (sexp-replace-sexp-beginning-by
+                 42
+                 'x
+                 (lambda (sexp) (declare (ignore sexp)) 'replaced)))
+
+  ;; nil input
+  (parachute:is eq
+                nil
+                (sexp-replace-sexp-beginning-by
+                 nil
+                 'x
+                 (lambda (sexp) (declare (ignore sexp)) 'replaced))))
+
+;;; end
